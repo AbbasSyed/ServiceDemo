56ae7ad6a24577c175cd27676aa54ef127045408
AbbasSyed <asyed1000@gmail.com>
2015-01-04 01:26:33 +0500

    updated to original-code

--- app/src/main/java/com/example/panacloud/myservicedemo/MainActivity.java
+++ app/src/main/java/com/example/panacloud/myservicedemo/MainActivity.java
@@ -1,89 +1,67 @@
 package com.example.panacloud.myservicedemo;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.ListView;
+import java.util.ArrayList;
 
-import java.io.IOException;
-
+/**
+ * Created by panacloud on 12/28/14.
+ */
 
 public class MainActivity extends Activity {
-    Button mButtonStart,mButtonStop;
-    RelativeLayout relativeLayout;
-    ImageView imageView;
-    private ActionBar actionBar;
-    static Bitmap bitmapScreenShot;
-    boolean b;
-    MediaPlayer mp;
+    ListView mListView;
+    Button mButton;
+    String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButtonStart = (Button) findViewById(R.id.button);
-        mButtonStop = (Button) findViewById(R.id.button2);
-        imageView = (ImageView) findViewById(R.id.imageView2);
-        actionBar = getActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        actionBar.setCustomView(R.layout.mall_actionbar_layout);
+        mListView = (ListView) findViewById(R.id.listView);
+        mButton = (Button) findViewById(R.id.button);
 
-        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("http://stream.wmbr.org:8000/med");
+        arrayList.add("http://marconi.emerson.edu:8000/wers");
+        arrayList.add("http://audio4.radioreference.com/161117976.mp3");
+        arrayList.add("http://brown.netexpress.com:8010/PALH");
+        arrayList.add("http://audio4.radioreference.com/516866891.mp3");
+        arrayList.add("http://relay.broadcastify.com:80/446184308");
+        final ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
+        mListView.setAdapter(myAdapter);
 
-        mp = new MediaPlayer();
-
-
-        mButtonStart.setOnClickListener(new View.OnClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                mp.start();
-//                b=true;
-//                startActivity(new Intent(getApplicationContext(), ScreenShotActivity.class));
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                URL = myAdapter.getItem(position);
+
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                i.putExtra("URL Stream",URL);
+
+                //Starts a service by passing in an Intent with some data
+                //multiple calls using startService will only call the same Service
+                startService(i);
             }
         });
 
-        mButtonStop.setOnClickListener(new View.OnClickListener() {
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b=false;
-                startActivity(new Intent(getApplicationContext(), ScreenShotActivity.class));
+
+                //Stops a running Service associated with this Android Application(activity)
+                stopService(new Intent(getApplicationContext(), MyService.class));
             }
         });
-
-//        imageView.setImageBitmap(bitmapScreenShot);
-
-        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try {
-            mp.setDataSource("http://stream.wmbr.org:8000/med");
-            mp.prepareAsync();
-        } catch (IOException e) {
-
-        }
 
     }
 
 
-    @Override
-    protected void onPause() {
 
-        if(b){
-            relativeLayout.setDrawingCacheEnabled(true);
-            bitmapScreenShot = Bitmap.createBitmap(relativeLayout.getDrawingCache());
-        }
-        else{
-            actionBar.getCustomView().setDrawingCacheEnabled(true);
-            bitmapScreenShot = Bitmap.createBitmap(actionBar.getCustomView().getDrawingCache());
-        }
-
-
-        super.onPause();
-    }
 }
--- app/src/main/java/com/example/panacloud/myservicedemo/MyService.java
+++ app/src/main/java/com/example/panacloud/myservicedemo/MyService.java
@@ -1,26 +1,68 @@
 package com.example.panacloud.myservicedemo;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+import java.io.IOException;
 
 /**
- * Created by panacloud on 12/26/14.
+ * Created by panacloud on 12/28/14.
  */
+
 public class MyService extends Service {
+    private MediaPlayer mp; //MediaPlayer used to play streamed music
+    String URL;
 
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
-    @Override
+    @Override //the Call to startService() runs the onStartCommand from where the Service begins its process.
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
 
+        //retrieving data from an Intent which was passed through the call to startService
+        URL = intent.getStringExtra("URL Stream");
+
+
+
+
+        mp = new MediaPlayer();
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mp.setDataSource(URL);
+            mp.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        Notification notification = new Notification(R.drawable.ic_launcher, "Service Running",System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(this, "Service is Running",
+                URL, pendingIntent);
+        //A known bug in Android v4.4 KitKat requires the Service to show up in the Foreground (System Notification bar)
+        startForeground(Notification.FLAG_ONLY_ALERT_ONCE, notification);
+
+
+
+        //A constant provided from the Services class which tells the Android System
+        //how to proceed with the service in case it is killed or restarted.
+        return Service.START_REDELIVER_INTENT;
     }
 
-    @Override
+    @Override //used to bind the Application to the Service
     public IBinder onBind(Intent intent) {
         return null;
     }
@@ -28,7 +70,10 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mp.release();
+        mp=null;
 
+        //stopSelf() is used when the service has completed its process and needs to stop by itself without its Activity.
+//        stopSelf();
     }
-
 }
